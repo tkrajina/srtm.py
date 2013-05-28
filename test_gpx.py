c675e617805e891853707838fc8323e917c1036e
Tomo Krajina <tkrajina@gmail.com>
2013-05-28 05:54:20 +0200

    test_gpx fixed

--- test_gpx.py
+++ test_gpx.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+Example on how to add elevations to existing GPX file
+"""
+
 import gpxpy as mod_gpxpy
 
-import srtm.retriever  as mod_retriever
+import srtm as mod_srtm
 
-with open('giro-del-horizonte-2013.gpx') as f:
-    gpx_contents = f.read()
+gpx_contents = """<?xml version="1.0"?><gpx creator="GPS Visualizer http://www.gpsvisualizer.com/" version="1.0" xmlns="http://www.topografix.com/GPX/1/0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd"><trk><name>test</name><trkseg><trkpt lat="47.0" lon="13.0"><ele>2714.0</ele></trkpt><trkpt lat="47.01" lon="13.0"><ele>2652.0</ele></trkpt><trkpt lat="47.02" lon="13.0"></trkpt><trkpt lat="47.03" lon="13.0"><ele>2414.0</ele></trkpt><trkpt lat="47.04" lon="13.0"><ele>2679.0</ele></trkpt><trkpt lat="47.05" lon="13.0"><ele>2710.0</ele></trkpt><trkpt lat="47.06" lon="13.0"><ele>2145.0</ele></trkpt><trkpt lat="47.07" lon="13.0"><ele>2004.0</ele></trkpt><trkpt lat="47.08" lon="13.0"><ele>1782.0</ele></trkpt><trkpt lat="47.09" lon="13.0"><ele>1592.0</ele></trkpt></trkseg><trkseg><trkpt lat="47.0" lon="13.0"><ele>2714.0</ele></trkpt><trkpt lat="47.0" lon="13.01"><ele>2522.0</ele></trkpt><trkpt lat="47.0" lon="13.02"><ele>2421.0</ele></trkpt><trkpt lat="47.0" lon="13.03"><ele>2099.0</ele></trkpt><trkpt lat="47.0" lon="13.04"><ele>1689.0</ele></trkpt><trkpt lat="47.0" lon="13.05"><ele>2080.0</ele></trkpt><trkpt lat="47.0" lon="13.06"><ele>2279.0</ele></trkpt><trkpt lat="47.0" lon="13.07"></trkpt><trkpt lat="47.0" lon="13.08"><ele>2343.0</ele></trkpt><trkpt lat="47.0" lon="13.09"><ele>1973.0</ele></trkpt></trkseg></trk></gpx>"""
 
 gpx = mod_gpxpy.parse(gpx_contents)
 
-geo_elevation_data = mod_retriever.get_geo_elevation_data()
+geo_elevation_data = mod_srtm.get_data()
 
 for segment_no, segment in enumerate(gpx.tracks[0].segments):
     for point in segment.points:
