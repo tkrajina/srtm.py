56aadf1064e25f8eecc066440c01792eafb7917d
Tomo Krajina <tkrajina@gmail.com>
2013-05-28 05:36:37 +0200

    License

--- srtm/data.py
+++ srtm/data.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 
+# Copyright 2013 Tomo Krajina
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Classess containing parsed elevation data.
 """
@@ -171,7 +185,6 @@ class GeoElevationFile:
 
         mod_logging.debug('{0}, {1} -> {2}'.format(row, column, i))
 
-        pdb.set_trace()
         byte_1 = ord(self.data[i * 2])
         byte_2 = ord(self.data[i * 2 + 1])
 
