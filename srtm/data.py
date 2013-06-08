81bc73f74bcacf69cbb654b6d074419f1f5deb11
Tomo Krajina <tkrajina@gmail.com>
2013-06-08 22:55:38 +0200

    Removed some debug output

--- srtm/data.py
+++ srtm/data.py
@@ -58,8 +58,8 @@ class GeoElevationData:
     def get_elevation(self, latitude, longitude, approximate=None):
         geo_elevation_file = self.get_file(float(latitude), float(longitude))
 
-        mod_logging.debug('File for ({0}, {1}) -> {2}'.format(
-                          latitude, longitude, geo_elevation_file))
+        #mod_logging.debug('File for ({0}, {1}) -> {2}'.format(
+        #                  latitude, longitude, geo_elevation_file))
 
         if not geo_elevation_file:
             return None
@@ -91,13 +91,15 @@ class GeoElevationData:
 
     def retrieve_or_load_file_data(self, file_name):
         data_file_name = '{0}/{1}'.format(self.files_directory, file_name)
+        zip_data_file_name = '{0}/{1}.zip'.format(self.files_directory, file_name)
 
         if mod_path.exists(data_file_name):
-            f = open(data_file_name)
-            data = f.read()
-            f.close()
-
-            return data
+            with open(data_file_name) as f:
+                return f.read()
+        elif mod_path.exists(zip_data_file_name):
+            with open(zip_data_file_name) as f:
+                data = f.read()
+                return mod_utils.unzip(data)
 
         url = None
 
@@ -107,13 +109,13 @@ class GeoElevationData:
             url = self.srtm3_files[file_name]
 
         if not url:
-            mod_logging.error('No file found: {0}'.format(file_name))
+            #mod_logging.error('No file found: {0}'.format(file_name))
             return None
 
         f = mod_urllib.urlopen(url)
         mod_logging.info('Retrieving {0}'.format(url))
         data = f.read()
-        mod_logging.info('Retrieved {0}'.format(url))
+        mod_logging.info('Retrieved {0} ({1} bytes)'.format(url, len(data)))
         f.close()
 
         if not data:
@@ -126,13 +128,14 @@ class GeoElevationData:
             data = self._reduce_file(data, file_name)
             data = mod_utils.zip(data, file_name)
 
-        with open(data_file_name, 'w') as f:
-            if self.leave_zipped:
-                f = open(data_file_name + '.zip', 'w')
-            else:
+        if self.leave_zipped:
+            with open(data_file_name + '.zip', 'w') as f:
+                f.write(data)
                 data = mod_utils.unzip(data)
-                f = open(data_file_name, 'w')
-            f.write(data)
+        else:
+            with open(data_file_name, 'w') as f:
+                data = mod_utils.unzip(data)
+                f.write(data)
 
         return data
 
@@ -171,7 +174,7 @@ class GeoElevationData:
                                       east_west, str(int(abs(mod_math.floor(longitude)))).zfill(3))
 
         if not self.srtm1_files.has_key(file_name) and not self.srtm3_files.has_key(file_name):
-            mod_logging.debug('No file found for ({0}, {1}) (file_name: {2})'.format(latitude, longitude, file_name))
+            #mod_logging.debug('No file found for ({0}, {1}) (file_name: {2})'.format(latitude, longitude, file_name))
             return None
 
         return file_name
@@ -327,7 +330,7 @@ class GeoElevationFile:
         i = row * (self.square_side) + column
         assert i < len(self.data) - 1
 
-        mod_logging.debug('{0}, {1} -> {2}'.format(row, column, i))
+        #mod_logging.debug('{0}, {1} -> {2}'.format(row, column, i))
 
         byte_1 = ord(self.data[i * 2])
         byte_2 = ord(self.data[i * 2 + 1])
